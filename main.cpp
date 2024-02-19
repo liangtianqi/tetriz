21239cf304d4b90fe63116c2f13bf3ed48a48daa
liangtianqi <1823180375@qq.com>
2024-02-19 18:15:28 +0800

    更新模块化，增加帧率显示

--- main.cpp
+++ main.cpp
@@ -1,10 +1,67 @@
 #include <iostream>
+#include <thread>
+#include "terminal.h"
+#include "utils.h"
+using namespace std::chrono_literals;
 
-using namespace std;
 
-void a();
+
+
+void init(){
+     tc::hide_cursor();
+}
+
+void loop(){
+    int i=1;
+    while(true){
+        
+        tc::clean_screen();
+
+        tc::move_to(1,1);
+        std::cout<<"FPS:"<<ut::fps();
+        
+        tc::move_to(i++%20,10);
+        tc::set_back_color(15);
+        std::cout<<"  ";
+        tc::reset_color();
+        std::cout<<std::flush;
+        std::this_thread::sleep_for(100ms); //s,ms,ns
+    }
+
+
+}
+
+void exit(){
+
+
+}
+
+
 int main(){
-    cout<<"hello world"<<endl;
-    a();
+    // tc::move_to(5,10);
+    // tc::set_fore_color(214);
+    // std::cout<<"hello world";
+    // tc::move_to(10,1);
+
+    init();
+    loop();
+    exit();
+
+
+    // tc::hide_cursor();
+    // int i=1;
+    // while(true){
+
+    //     tc::clean_screen();
+    //     tc::move_to(i++,10);
+    //     tc::set_back_color(15);
+    //     std::cout<<"  ";
+    //     tc::reset_color();
+    //     std::cout<<std::flush;
+    //     std::this_thread::sleep_for(1s); //s,ms,ns
+    // }
+
+
+
     return 0;
 }
